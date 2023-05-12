eecb63a7f507828858936b1e544899b896d6e32b
Lehik Test <lehik33@yandex.ru>
2023-05-12 12:58:19 +0300

    Code reformated in Test

--- src/test/java/ru/netology/services/VacationCalculateTest.java
+++ src/test/java/ru/netology/services/VacationCalculateTest.java
@@ -14,6 +14,7 @@ public class VacationCalculateTest {
         Assertions.assertEquals(expected, actual);
 
     }
+
     @Test
     public void testMonthVacation2() {
         VacationCalculate service = new VacationCalculate();
